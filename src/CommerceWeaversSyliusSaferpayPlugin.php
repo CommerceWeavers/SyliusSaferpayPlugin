5c940a4adcb79b3abf7a5d78c1edcb08127b9d8b
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-04-03 15:01:35 +0200

    Clean and adjust the initialized plugin

--- src/CommerceWeaversSyliusSaferpayPlugin.php
+++ src/CommerceWeaversSyliusSaferpayPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace CommerceWeavers\SyliusSaferpayPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class CommerceWeaversSyliusSaferpayPlugin extends Bundle
 {
     use SyliusPluginTrait;
 
--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace CommerceWeavers\SyliusSaferpayPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('acme_sylius_example_plugin');
+        $treeBuilder = new TreeBuilder('commerce_weavers_sylius_saferpay');
         $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
