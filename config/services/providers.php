b424b61525d0bcc2e3d60e6b6c032715e077bb52
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-04-11 15:01:16 +0200

    Add UuidProvider service

---
+++ config/services/providers.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use CommerceWeavers\SyliusSaferpayPlugin\Client\SaferpayClientInterface;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Action\AssertAction;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Action\AuthorizeAction;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Action\CaptureAction;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Action\ResolveNextRouteAction;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Factory\SaferpayGatewayFactory;
+use CommerceWeavers\SyliusSaferpayPlugin\Provider\UuidProvider;
+use CommerceWeavers\SyliusSaferpayPlugin\Provider\UuidProviderInterface;
+use Payum\Core\Bridge\Symfony\Builder\GatewayFactoryBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+return static function (ContainerConfigurator $containerConfigurator) {
+    $services = $containerConfigurator->services();
+
+    $services->set(UuidProviderInterface::class, UuidProvider::class);
+
+    $services
+        ->set(AuthorizeAction::class)
+        ->public()
+        ->args([
+            service(SaferpayClientInterface::class)
+        ])
+        ->tag('payum.action', ['factory' => 'saferpay', 'alias' => 'payum.action.authorize'])
+    ;
+
+    $services
+        ->set(AssertAction::class)
+        ->public()
+        ->args([
+            service('sylius.http_client')
+        ])
+        ->tag('payum.action', ['factory' => 'saferpay', 'alias' => 'payum.action.assert'])
+    ;
+
+    $services
+        ->set(CaptureAction::class)
+        ->public()
+        ->args([
+            service(SaferpayClientInterface::class)
+        ])
+        ->tag('payum.action', ['factory' => 'saferpay', 'alias' => 'payum.action.capture'])
+    ;
+
+    $services
+        ->set(ResolveNextRouteAction::class)
+        ->public()
+        ->tag('payum.action', ['factory' => 'saferpay', 'alias' => 'payum.action.resolve_next_route'])
+    ;
+};
