3970e855bc2580279d26c57eb40f8a741f41f3ee
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-04-11 14:48:57 +0200

    Change notation of configuration parameters + add use_authorize

--- src/Form/Type/SaferpayGatewayConfigurationType.php
+++ src/Form/Type/SaferpayGatewayConfigurationType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CommerceWeavers\SyliusSaferpayPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,8 +17,9 @@ final class SaferpayGatewayConfigurationType extends AbstractType
         $builder
             ->add('username', TextType::class, ['label' => 'sylius.ui.username'])
             ->add('password', PasswordType::class, ['label' => 'sylius.ui.password'])
-            ->add('customerId', TextType::class, ['label' => 'sylius_saferpay.ui.customer_id'])
-            ->add('terminalId', TextType::class, ['label' => 'sylius_saferpay.ui.terminal_id'])
+            ->add('customer_id', TextType::class, ['label' => 'sylius_saferpay.ui.customer_id'])
+            ->add('terminal_id', TextType::class, ['label' => 'sylius_saferpay.ui.terminal_id'])
+            ->add('use_authorize', HiddenType::class, ['data' => true])
         ;
     }
 }
