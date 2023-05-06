07854e6f18567ac18fab3e8c77ad845987ff9be9
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-06 08:40:48 +0200

    Implement PaymentAssertionFailureListener

---
+++ src/TransactionLog/EventListener/PaymentAssertionFailureListener.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener;
+
+use CommerceWeavers\SyliusSaferpayPlugin\Client\Event\PaymentAssertionFailed;
+use CommerceWeavers\SyliusSaferpayPlugin\Factory\TransactionLogFactoryInterface;
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\Exception\PaymentNotFoundException;
+use Doctrine\Persistence\ObjectManager;
+use Sylius\Calendar\Provider\DateTimeProviderInterface;
+use Sylius\Component\Core\Repository\PaymentRepositoryInterface;
+
+final class PaymentAssertionFailureListener
+{
+    public function __construct (
+        private TransactionLogFactoryInterface $transactionLogFactory,
+        private ObjectManager $transactionLogObjectManager,
+        private PaymentRepositoryInterface $paymentRepository,
+        private DateTimeProviderInterface $dateTimeProvider,
+    ) {
+    }
+
+    /**
+     * @throws PaymentNotFoundException
+     */
+    public function __invoke(PaymentAssertionFailed $event): void
+    {
+        $payment = $this->paymentRepository->find($event->getPaymentId());
+
+        if (null === $payment) {
+            throw new PaymentNotFoundException($event->getPaymentId());
+        }
+
+        $transactionLog = $this->transactionLogFactory->create(
+            $this->dateTimeProvider->now(),
+            $payment,
+            'PaymentAssertionFailed',
+            [
+                'url' => $event->getRequestUrl(),
+                'request' => $event->getRequestBody(),
+                'response' => $event->getResponseData(),
+            ],
+            'error',
+        );
+
+        $this->transactionLogObjectManager->persist($transactionLog);
+        $this->transactionLogObjectManager->flush();
+    }
+}
