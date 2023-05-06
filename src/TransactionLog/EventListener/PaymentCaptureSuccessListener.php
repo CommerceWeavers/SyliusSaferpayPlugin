2611cd8872a09c8adafdb9f0b77ec46b0c2ab085
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-06 10:15:31 +0200

    Implement PaymentCaptureSuccessListener

---
+++ src/TransactionLog/EventListener/PaymentCaptureSuccessListener.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener;
+
+use CommerceWeavers\SyliusSaferpayPlugin\Client\Event\PaymentCaptureSucceeded;
+use CommerceWeavers\SyliusSaferpayPlugin\Factory\TransactionLogFactoryInterface;
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\Exception\PaymentNotFoundException;
+use Doctrine\Persistence\ObjectManager;
+use Sylius\Calendar\Provider\DateTimeProviderInterface;
+use Sylius\Component\Core\Repository\PaymentRepositoryInterface;
+
+final class PaymentCaptureSuccessListener
+{
+    public function __construct(
+        private TransactionLogFactoryInterface $transactionLogFactory,
+        private ObjectManager $transactionLogObjectManager,
+        private PaymentRepositoryInterface $paymentRepository,
+        private DateTimeProviderInterface $dateTimeProvider,
+    ) {
+    }
+
+    /**
+     * @throws PaymentNotFoundException
+     */
+    public function __invoke(PaymentCaptureSucceeded $event): void
+    {
+        $payment = $this->paymentRepository->find($event->getPaymentId());
+
+        if (null === $payment) {
+            throw new PaymentNotFoundException($event->getPaymentId());
+        }
+
+        $transactionLog = $this->transactionLogFactory->create(
+            $this->dateTimeProvider->now(),
+            $payment,
+            'Payment capture succeeded',
+            [
+                'url' => $event->getRequestUrl(),
+                'request' => $event->getRequestBody(),
+                'response' => $event->getResponseData(),
+            ],
+            'info',
+        );
+
+        $this->transactionLogObjectManager->persist($transactionLog);
+        $this->transactionLogObjectManager->flush();
+    }
+}
