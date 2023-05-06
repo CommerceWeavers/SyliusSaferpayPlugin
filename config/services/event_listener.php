bd28f2838b4b7df0a8a6af632952cc6794dcc325
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-06 10:15:43 +0200

    Register TransactionLog event listeners

--- config/services/event_listener.php
+++ config/services/event_listener.php
@@ -2,8 +2,53 @@
 
 declare(strict_types=1);
 
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\PaymentAssertionFailureListener;
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\PaymentAssertionSuccessListener;
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\PaymentAuthorizationSuccessListener;
+use CommerceWeavers\SyliusSaferpayPlugin\TransactionLog\EventListener\PaymentCaptureSuccessListener;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $containerConfigurator) {
     $services = $containerConfigurator->services();
+
+    $services->set(PaymentAssertionFailureListener::class)
+        ->args([
+            service('commerce_weavers_saferpay.factory.transaction_log'),
+            service('commerce_weavers_saferpay.manager.transaction_log'),
+            service('sylius.repository.payment'),
+            service('sylius.calendar'),
+        ])
+        ->tag('messenger.message_handler', ['bus' => 'sylius.event_bus'])
+    ;
+
+    $services->set(PaymentAssertionSuccessListener::class)
+        ->args([
+            service('commerce_weavers_saferpay.factory.transaction_log'),
+            service('commerce_weavers_saferpay.manager.transaction_log'),
+            service('sylius.repository.payment'),
+            service('sylius.calendar'),
+        ])
+        ->tag('messenger.message_handler', ['bus' => 'sylius.event_bus'])
+    ;
+
+    $services->set(PaymentAuthorizationSuccessListener::class)
+        ->args([
+            service('commerce_weavers_saferpay.factory.transaction_log'),
+            service('commerce_weavers_saferpay.manager.transaction_log'),
+            service('sylius.repository.payment'),
+            service('sylius.calendar'),
+        ])
+        ->tag('messenger.message_handler', ['bus' => 'sylius.event_bus'])
+    ;
+
+    $services->set(PaymentCaptureSuccessListener::class)
+        ->args([
+            service('commerce_weavers_saferpay.factory.transaction_log'),
+            service('commerce_weavers_saferpay.manager.transaction_log'),
+            service('sylius.repository.payment'),
+            service('sylius.calendar'),
+        ])
+        ->tag('messenger.message_handler', ['bus' => 'sylius.event_bus'])
+    ;
 };
