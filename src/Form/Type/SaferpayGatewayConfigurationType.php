0bc3f50c9e99e25daf83b4d3b2941877ebbfb7af
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-08 16:38:11 +0200

    Add choosing available payment methods for Saferpay while configuring gateway

--- src/Form/Type/SaferpayGatewayConfigurationType.php
+++ src/Form/Type/SaferpayGatewayConfigurationType.php
@@ -6,6 +6,7 @@ namespace CommerceWeavers\SyliusSaferpayPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,6 +22,45 @@ final class SaferpayGatewayConfigurationType extends AbstractType
             ->add('customer_id', TextType::class, ['label' => 'commerce_weavers_saferpay.ui.customer_id'])
             ->add('terminal_id', TextType::class, ['label' => 'commerce_weavers_saferpay.ui.terminal_id'])
             ->add('sandbox', CheckboxType::class, ['label' => 'commerce_weavers_saferpay.ui.sandbox'])
+            ->add('customer_id', TextType::class, ['label' => 'sylius_saferpay.ui.customer_id'])
+            ->add('terminal_id', TextType::class, ['label' => 'sylius_saferpay.ui.terminal_id'])
+            ->add('allowed_payment_methods', ChoiceType::class, [
+                'attr' => [
+                    'class' => 'saferpay-allowed-payment-methods',
+                ],
+                'choices' => [
+                    'Account to Account' => 'ACCOUNTTOACCOUNT',
+                    'Alipay' => 'ALIPAY',
+                    'American Express' => 'AMEX',
+                    'Bancontact' => 'BANCONTACT',
+                    'Bonus' => 'BONUS',
+                    'Diners Club' => 'DINERS',
+                    'Direct Debit' => 'DIRECTDEBIT',
+                    'ePrzelewy' => 'EPRZELEWY',
+                    'EPS' => 'EPS',
+                    'Giropay' => 'GIROPAY',
+                    'iDEAL' => 'IDEAL',
+                    'Invoice' => 'INVOICE',
+                    'JCB' => 'JCB',
+                    'Klarna' => 'KLARNA',
+                    'Maestro' => 'MAESTRO',
+                    'Mastercard' => 'MASTERCARD',
+                    'MyOne' => 'MYONE',
+                    'Payconiq' => 'PAYCONIQ',
+                    'Paydirekt' => 'PAYDIREKT',
+                    'PayPal' => 'PAYPAL',
+                    'Postcard' => 'POSTCARD',
+                    'PostFinance' => 'POSTFINANCE',
+                    'SOFORT' => 'SOFORT',
+                    'TWINT' => 'TWINT',
+                    'UnionPay' => 'UNIONPAY',
+                    'Visa' => 'VISA',
+                    'WLCryptoPayments' => 'WLCRYPTOPAYMENTS',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+            ])
+            ->add('sandbox', CheckboxType::class, ['label' => 'sylius_saferpay.ui.sandbox'])
             ->add('use_authorize', HiddenType::class, ['data' => true])
         ;
     }
