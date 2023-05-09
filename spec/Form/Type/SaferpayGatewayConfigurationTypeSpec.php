9caef7baaf02524456205dd39a2a63726ba2434e
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-09 12:18:10 +0200

    Fix nulling Saferpay's password configuration on save

---
+++ spec/Form/Type/SaferpayGatewayConfigurationTypeSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\CommerceWeavers\SyliusSaferpayPlugin\Form\Type;
+
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormInterface;
+
+final class SaferpayGatewayConfigurationTypeSpec extends ObjectBehavior
+{
+    function it_should_disable_password_field_mapping_if_no_passed_along_data(FormInterface $form): void
+    {
+        $event = new PreSubmitEvent($form->getWrappedObject(), []);
+
+        $form->remove('password')->willReturn($form)->shouldBeCalled();
+        $form->add('password', PasswordType::class, ['label' => 'sylius.ui.password', 'mapped' => false])->shouldBeCalled();
+
+        $this->onPreSubmit($event);
+    }
+
+    function it_should_disable_password_field_mapping_if_password_is_null(FormInterface $form): void
+    {
+        $event = new PreSubmitEvent($form->getWrappedObject(), ['password' => null]);
+
+        $form->remove('password')->willReturn($form)->shouldBeCalled();
+        $form->add('password', PasswordType::class, ['label' => 'sylius.ui.password', 'mapped' => false])->shouldBeCalled();
+
+        $this->onPreSubmit($event);
+    }
+
+    function it_should_disable_password_field_mapping_if_password_is_empty(FormInterface $form): void
+    {
+        $event = new PreSubmitEvent($form->getWrappedObject(), ['password' => '']);
+
+        $form->remove('password')->willReturn($form)->shouldBeCalled();
+        $form->add('password', PasswordType::class, ['label' => 'sylius.ui.password', 'mapped' => false])->shouldBeCalled();
+
+        $this->onPreSubmit($event);
+    }
+
+    function it_should_not_disable_password_field_mapping_if_password_is_not_empty(FormInterface $form): void
+    {
+        $event = new PreSubmitEvent($form->getWrappedObject(), ['password' => 'not empty']);
+
+        $form->remove('password')->willReturn($form)->shouldNotBeCalled();
+        $form->add('password', PasswordType::class, ['label' => 'sylius.ui.password', 'mapped' => false])->shouldNotBeCalled();
+
+        $this->onPreSubmit($event);
+    }
+}
--- src/Form/Type/SaferpayGatewayConfigurationType.php
+++ src/Form/Type/SaferpayGatewayConfigurationType.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace CommerceWeavers\SyliusSaferpayPlugin\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 
 final class SaferpayGatewayConfigurationType extends AbstractType
 {
@@ -61,5 +63,19 @@ final class SaferpayGatewayConfigurationType extends AbstractType
             ->add('sandbox', CheckboxType::class, ['label' => 'commerce_weavers_saferpay.ui.sandbox'])
             ->add('use_authorize', HiddenType::class, ['data' => true])
         ;
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'onPreSubmit']);
+    }
+
+    public function onPreSubmit(PreSubmitEvent $event): void
+    {
+        /** @var array<string, mixed> $data */
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        if (!isset($data['password']) || '' === $data['password']) {
+            $form->remove('password');
+            $form->add('password', PasswordType::class, ['label' => 'sylius.ui.password', 'mapped' => false]);
+        }
     }
 }
