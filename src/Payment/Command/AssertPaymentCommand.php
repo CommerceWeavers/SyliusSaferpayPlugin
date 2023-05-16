e32d4762c283dbe7177b5da2ea5f3a19257dde58
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-05-16 11:03:52 +0200

    Create AssertPaymentHandler

---
+++ src/Payment/Command/AssertPaymentCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommerceWeavers\SyliusSaferpayPlugin\Payment\Command;
+
+final class AssertPaymentCommand
+{
+    public function __construct(
+        private string $payumToken,
+    ) {
+    }
+
+    public function getPayumToken(): string
+    {
+        return $this->payumToken;
+    }
+}
---
+++ src/Payment/Command/Handler/AssertPaymentHandler.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommerceWeavers\SyliusSaferpayPlugin\Payment\Command\Handler;
+
+use CommerceWeavers\SyliusSaferpayPlugin\Payment\Command\AssertPaymentCommand;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Factory\AssertFactoryInterface;
+use CommerceWeavers\SyliusSaferpayPlugin\Payum\Factory\ResolveNextCommandFactoryInterface;
+use Payum\Core\Payum;
+use Payum\Core\Storage\StorageInterface;
+use Sylius\Bundle\PayumBundle\Factory\GetStatusFactoryInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DispatchAfterCurrentBusStamp;
+use Webmozart\Assert\Assert;
+
+final class AssertPaymentHandler
+{
+    public function __construct(
+        private MessageBusInterface $commandBus,
+        private Payum $payum,
+        private StorageInterface $tokenStorage,
+        private AssertFactoryInterface $assertFactory,
+        private GetStatusFactoryInterface $getStatusRequestFactory,
+        private ResolveNextCommandFactoryInterface $resolveNextCommandFactory,
+    ) {
+    }
+
+    public function __invoke(AssertPaymentCommand $command): void
+    {
+        $token = $this->tokenStorage->find($command->getPayumToken());
+        Assert::notNull($token, 'Token not found.');
+
+        $gateway = $this->payum->getGateway($token->getGatewayName());
+
+        $assert = $this->assertFactory->createNewWithModel($token);
+        $gateway->execute($assert);
+
+        $status = $this->getStatusRequestFactory->createNewWithModel($assert->getFirstModel());
+        $gateway->execute($status);
+
+        $this->tokenStorage->delete($token);
+
+        $resolvedNextCommand = $this->resolveNextCommandFactory->createNewWithModel($assert->getFirstModel());
+        $gateway->execute($resolvedNextCommand);
+
+        if (null === $resolvedNextCommand->getNextCommand()) {
+            return;
+        }
+
+        $this->commandBus->dispatch($resolvedNextCommand->getNextCommand(), [new DispatchAfterCurrentBusStamp()]);
+    }
+}
