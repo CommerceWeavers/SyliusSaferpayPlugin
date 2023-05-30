ba33de0fffe27ad740a5fc8a3067af969ed568f6
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-05-30 07:43:31 +0200

    Revert overriding payment resource controller

---
+++ config/services/controllers.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use CommerceWeavers\SyliusSaferpayPlugin\Controller\PaymentController;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\inline_service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+return static function (ContainerConfigurator $containerConfigurator) {
+    $services = $containerConfigurator->services();
+
+    $services
+        ->set(PaymentController::class)
+        ->decorate('sylius.controller.payment')
+        ->args([
+            service('.inner'),
+            inline_service(MetadataInterface::class)
+                ->factory([service('sylius.resource_registry'), 'get'])
+                ->args(['sylius.payment']),
+            service('sylius.resource_controller.request_configuration_factory'),
+            service('sylius.resource_controller.redirect_handler'),
+        ])
+    ;
+};
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -5,17 +5,58 @@ declare(strict_types=1);
 namespace CommerceWeavers\SyliusSaferpayPlugin\Controller;
 
 use CommerceWeavers\SyliusSaferpayPlugin\Payum\Exception\PaymentRefundFailedException;
-use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Bundle\ResourceBundle\Controller\RedirectHandlerInterface;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController as BaseResourceController;
+use Sylius\Component\Resource\Metadata\MetadataInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-final class PaymentController extends ResourceController
+final class PaymentController
 {
+    public function __construct(
+        private BaseResourceController $decoratedResourceController,
+        private MetadataInterface $metadata,
+        private RequestConfigurationFactoryInterface $requestConfigurationFactory,
+        private RedirectHandlerInterface $redirectHandler,
+    ) {
+    }
+
+    public function showAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->showAction($request);
+    }
+
+    public function indexAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->indexAction($request);
+    }
+
+    public function createAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->createAction($request);
+    }
+
+    public function updateAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->updateAction($request);
+    }
+
+    public function deleteAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->deleteAction($request);
+    }
+
+    public function bulkDeleteAction(Request $request): Response
+    {
+        return $this->decoratedResourceController->bulkDeleteAction($request);
+    }
+
     public function applyStateMachineTransitionAction(Request $request): Response
     {
         try {
-            return parent::applyStateMachineTransitionAction($request);
+            return $this->decoratedResourceController->applyStateMachineTransitionAction($request);
         } catch (PaymentRefundFailedException) {
             $this->addFlashMessage($request, 'error', 'sylius_saferpay.payment.refund_failed');
 
