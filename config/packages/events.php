64e063960efa72fc8a07be3bd1be2c6c0475c99e
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-05-31 07:14:24 +0200

    Refactor configure payment methods template to use Sylius Template Events

--- config/packages/events.php
+++ config/packages/events.php
@@ -14,6 +14,50 @@ return static function (ContainerConfigurator $containerConfigurator) {
                     ],
                 ],
             ],
+            'commerce_weavers_saferpay.admin.payment_method.configure_payment_methods' => [
+                'blocks' => [
+                    'header' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/_header.html.twig',
+                        'priority' => 20,
+                    ],
+                    'content' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/_content.html.twig',
+                        'priority' => 10,
+                    ],
+                ],
+            ],
+            'commerce_weavers_saferpay.admin.payment_method.configure_payment_methods.header' => [
+                'blocks' => [
+                    'title' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/Header/_headerTitle.html.twig',
+                        'priority' => 20,
+                    ],
+                    'breadcrumb' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/Header/_breadcrumb.html.twig',
+                        'priority' => 10,
+                    ],
+                ],
+            ],
+            'commerce_weavers_saferpay.admin.payment_method.configure_payment_methods.form' => [
+                'blocks' => [
+                    'content' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/Form/_content.html.twig',
+                        'priority' => 10,
+                    ],
+                ],
+            ],
+            'commerce_weavers_saferpay.admin.payment_method.configure_payment_methods.form.content' => [
+                'blocks' => [
+                    'allowed_payment_methods' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/Form/_allowedPaymentMethods.html.twig',
+                        'priority' => 20,
+                    ],
+                    'buttons' => [
+                        'template' => '@CommerceWeaversSyliusSaferpayPlugin/Admin/PaymentMethod/ConfigurePaymentMethods/Form/_buttons.html.twig',
+                        'priority' => 10,
+                    ],
+                ],
+            ],
         ],
     ]);
 };
